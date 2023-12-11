3da159db26af2d73848327832edf464df1d5f0c8
Kavindu <kmvithanage48@gmail.com>
2023-12-11 22:59:37 +0530

    first commit

---
+++ division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    interger_division = a // b
+    float_division = a / b
+
+    print(interger_division)
+    print(float_division)
---
+++ loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    numbers = list(range(n))
+
+    for number in numbers:
+        print(pow(number, 2))
+
+
